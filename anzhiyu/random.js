6589e7675ee80045e83d71b6b3dc6e62d56c194d
231250099 <231250099@smail.nju.edu.cn>
2025-12-18 18:07:09 +0800

    Site updated: 2025-12-18 18:07:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/09/hello-world/"];function toRandomPost(){
+var posts=["2025/12/09/hello-world/","2025/12/10/Lebron-James/","2025/12/18/life/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
